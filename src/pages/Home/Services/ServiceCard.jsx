cc23ba75ecbecccecd3d14a3c3ec29551b52e161
Imam444 <mdimamhossain565528@gmail.com>
2024-12-23 00:11:47 +0600

    create checkout form  and load single service data

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const ServiceCard = ({ service }) => {
-    const { title, img, price } = service;
+    const {_id, title, img, price } = service;
     return (
         <div className="card bg-base-100 w-96 shadow-xl">
             <figure className="px-10 pt-10">
